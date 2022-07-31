6a2fa07ee4c43b48c201cc0cba2fda398d24ac8e
Jack <jack@Jack.jack>
2022-07-31 13:17:20 -0500

    siderbar

--- dashboard/pages/filter_title.py
+++ dashboard/pages/filter_title.py
@@ -2,6 +2,8 @@ import dash
 from dash_labs.plugins import register_page
 
 register_page(__name__, path="/filter_title")
+# LOAD THE DIFFERENT FILES
+from lib import sider_title
 
 from dash import Dash, dcc, html, Input, Output, callback
 import plotly.express as px
@@ -9,7 +11,7 @@ import plotly.express as px
 
 layout = html.Div(
     [
-        
+        sider_title.sidebar_title,
     ]
 )
 
