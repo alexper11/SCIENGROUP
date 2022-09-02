0e943e37eb77d3e9b6a1a9caebfdc8a1d2afb89f
Alex <alex.perdomo44@gmail.com>
2022-09-02 13:33:00 -0500

    agregada columna tipo a prototipo y software, creados modelos y controladores

--- cvlac/models/Prototipo.py
+++ cvlac/models/Prototipo.py
@@ -1,9 +1,9 @@
 from cvlac.db import Base
 from sqlalchemy import Column, Integer, String
 
-class Software(Base):
+class Prototipo(Base):
     
-    __tablename__ = 'software'
+    __tablename__ = 'prototipo'
     id = Column(Integer, primary_key=True)
     idcvlac = Column(String(20), nullable=False)
     autor = Column(String(4000), nullable=False)
@@ -12,8 +12,6 @@ class Software(Base):
     contrato_registro = Column(String(300), nullable=True)
     lugar = Column(String(50), nullable=True)
     fecha = Column(String(500), nullable=False)
-    plataforma = Column(String(2000), nullable=True)
-    ambiente = Column(String(150), nullable=True)
     palabras = Column(String(3000), nullable=True)
     areas = Column(String(3000), nullable=True)
     sectores = Column(String(3000), nullable=True)
@@ -24,7 +22,7 @@ class Software(Base):
             setattr(self, key, value)
             
     def __repr__(self):
-        return f'Software({self.nombre}, {self.idcvlac})'
+        return f'Prototipo({self.nombre}, {self.idcvlac})'
     
     def __str__(self):
         return self.idcvlac
