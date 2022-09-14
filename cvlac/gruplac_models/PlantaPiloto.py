3aa794a33890667539b383a23566b7420945a8e3
Alex <alex.perdomo44@gmail.com>
2022-09-14 15:39:41 -0500

    creados controladores y modelos de empresa_tecnologica, innovacion_empresarial y planta_piloto

---
+++ cvlac/gruplac_models/PlantaPiloto.py
@@ -0,0 +1,28 @@
+from cvlac.db_gruplac import Base
+from sqlalchemy import Column, Integer, String, Boolean
+
+class PlantaPiloto(Base):
+    
+    __tablename__ = 'planta_piloto'
+    id = Column(Integer, primary_key=True)
+    idgruplac = Column(String(50), nullable=False)
+    verificado = Column(Boolean,unique=False, default=True)
+    tipo = Column(String(200), nullable=True)
+    nombre = Column(String(1500), nullable=True)
+    lugar = Column(String(100), nullable=True)
+    fecha = Column(String(100), nullable=True)
+    disponibilidad = Column(String(100), nullable=True)
+    nombre_comercial = Column(String(1000), nullable=True)
+    institucion = Column(String(400), nullable=True)
+    autores = Column(String(6000), nullable=True)       
+    
+    def __init__(self, **kwargs):
+        
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+            
+    def __repr__(self):
+        return f'PlantaPiloto({self.nombre}, {self.idgruplac})'
+    
+    def __str__(self):
+        return self.idgruplac
