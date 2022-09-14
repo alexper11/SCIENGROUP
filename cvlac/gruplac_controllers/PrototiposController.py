6b0f1c7bb43c88536afd0a7710ea9aad15e78eb2
Alex <alex.perdomo44@gmail.com>
2022-09-14 14:47:09 -0500

    creados controladores y modelos diseno_industrial,otros_tecnologicos,prototipos y software

---
+++ cvlac/gruplac_controllers/PrototiposController.py
@@ -0,0 +1,30 @@
+from cvlac import db_gruplac
+import pandas
+from cvlac.gruplac_models.Prototipos import Prototipos
+
+class PrototiposController:
+    count = 0
+    def __init__(self):
+        self.__class__.count = self.__class__.count + 1
+    
+    def insert_df(self, df):
+        dicList=df.to_dict(orient='records')
+        db_gruplac.session.bulk_insert_mappings(Prototipos, dicList)
+        try:
+            db_gruplac.session.commit()
+        except:
+            db_gruplac.session.rollback()
+            print("No se pudo insertar el dataframe en Prototipos")
+            df.to_csv('PrototiposGruplac.csv')
+        finally:
+            db_gruplac.session.close()
+    
+    def delete_idgruplac(self, idgruplac):
+        db_gruplac.session.query(Prototipos).filter(Prototipos.idgruplac==idgruplac).delete()
+        try:
+            db_gruplac.session.commit()
+        except:
+            db_gruplac.session.rollback()
+            print("No se pudo eliminar el idgruplac: "+idgruplac+" en Prototipos")
+        finally:
+            db_gruplac.session.close()
