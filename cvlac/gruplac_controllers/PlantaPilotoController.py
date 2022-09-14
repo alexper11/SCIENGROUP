b28f52a1a20e54da6f08a24dea385bcf6c44c229
Alex <alex.perdomo44@gmail.com>
2022-09-14 15:39:41 -0500

    creados controladores y modelos de empresa_tecnologica, innovacion_empresarial y planta_piloto

---
+++ cvlac/gruplac_controllers/PlantaPilotoController.py
@@ -0,0 +1,30 @@
+from cvlac import db_gruplac
+import pandas
+from cvlac.gruplac_models.PlantaPiloto import PlantaPiloto
+
+class PlantaPilotoController:
+    count = 0
+    def __init__(self):
+        self.__class__.count = self.__class__.count + 1
+    
+    def insert_df(self, df):
+        dicList=df.to_dict(orient='records')
+        db_gruplac.session.bulk_insert_mappings(PlantaPiloto, dicList)
+        try:
+            db_gruplac.session.commit()
+        except:
+            db_gruplac.session.rollback()
+            print("No se pudo insertar el dataframe en PlantaPiloto")
+            df.to_csv('PlantaPilotoGruplac.csv')
+        finally:
+            db_gruplac.session.close()
+    
+    def delete_idgruplac(self, idgruplac):
+        db_gruplac.session.query(PlantaPiloto).filter(PlantaPiloto.idgruplac==idgruplac).delete()
+        try:
+            db_gruplac.session.commit()
+        except:
+            db_gruplac.session.rollback()
+            print("No se pudo eliminar el idgruplac: "+idgruplac+" en PlantaPiloto")
+        finally:
+            db_gruplac.session.close()
---
+++ cvlac/gruplac_models/EmpresaTecnologica.py
@@ -0,0 +1,27 @@
+from cvlac.db_gruplac import Base
+from sqlalchemy import Column, Integer, String, Boolean
+
+class EmpresaTecnologica(Base):
+    
+    __tablename__ = 'empresa_tecnologica'
+    id = Column(Integer, primary_key=True)
+    idgruplac = Column(String(50), nullable=False)
+    verificado = Column(Boolean,unique=False, default=True)
+    tipo = Column(String(200), nullable=True)
+    nombre = Column(String(1500), nullable=True)
+    fecha = Column(String(100), nullable=True)
+    nit = Column(String(150), nullable=True)
+    fecha_registro = Column(String(100), nullable=True)
+    mercado = Column(String(150), nullable=True)
+    autores = Column(String(6000), nullable=True)       
+    
+    def __init__(self, **kwargs):
+        
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+            
+    def __repr__(self):
+        return f'EmpresaTecnologica({self.nombre}, {self.idgruplac})'
+    
+    def __str__(self):
+        return self.idgruplac
