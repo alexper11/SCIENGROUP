444d8ef7b7de73c1ce1ef985a590061fe66ac602
Jack <jack@Jack.jack>
2022-09-19 11:13:03 -0500

    correccion gruplac

--- ToolScripts/explorer_gruplac.py
+++ ToolScripts/explorer_gruplac.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from pyquery import PyQuery as pq
 import os
-#from requests.packages.urllib3.exceptions import InsecureRequestWarning
-#requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 url_general="https://scienti.minciencias.gov.co/ciencia-war/busquedaGrupoXDepartamentoGrupo.do?codInst=&sglPais=COL&sgDepartamento=CA&maxRows=15&grupos_tr_=true&grupos_p_=1&grupos_mr_=117"
 
@@ -19,7 +19,7 @@ for a in links_total:
         for i in range(tries):
             try: 
                 headers = {'ruplac, headUser-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-                r = requests.get(url_gruplac, headers=headers)#, verify=False)
+                r = requests.get(url_gruplac, headers=headers, verify=False)
             except:
                 print("Error con :",url_gruplac)
                 continue
@@ -30,7 +30,7 @@ for a in links_total:
         for table in child:
             trs=table.find_all('tr')
             titulo=trs[0].find('td').text.strip()
-            if (len(trs) > 1) and (titulo=='Empresas de base tecnológica'):
+            if (len(trs) > 1) and (titulo=='Programa académico de maestría'):
                 print(url_gruplac)
                 count=count+1
 print('count : ',count)
