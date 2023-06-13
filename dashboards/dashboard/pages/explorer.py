17b35b2fca50e5c5a317520ce1387fd76f7f3f55
Alex <alex.perdomo44@gmail.com>
2023-06-13 12:58:48 -0500

    explorer v1

--- dashboards/dashboard/pages/explorer.py
+++ dashboards/dashboard/pages/explorer.py
@@ -17,21 +17,33 @@ from functions.csv_importer import gruplac_articulos, gruplac_basico, gruplac_ca
 
 elemento_seleccionado='Artículos'
 
-table_explorer= dcc.Loading([
+table_explorer= dcc.Loading([dbc.Toast(
+            id="auto-toast",
+            header="No hay datos disponibles",
+            color='secondary',
+            duration=4000,
+            is_open=False,
+            style={"color":"white",'backgroundColor': 'red','padding-top': '50px'}
+            ),
             # html.H3('Tabla de datos', id="title_table"),
             dash_table.DataTable(
                 id='table_date',
                 #columns=[{'name': i, 'id': i} for i in dataset_explorador.columns],
                 data = None,
                 page_size=100,
-                virtualization = True,
-                fixed_columns={'headers':True},
+                #virtualization = True,
+                #fixed_rows={'headers':True,'data':0},
                 cell_selectable=False,
-                #page_action='none',
-                # css=[{'rule':'height:inherit'}],
-                style_table={'overflowX':'auto', 'height': '90%','width':'90%', 'maxWidth': 'auto', 'maxHeight':'100%'},
+                style_header={'position': 'sticky', 'top': 0},
+                style_table={'overflowX':'auto', 'maxHeight':'80vh','maxWidth':'95vw'},
+                style_cell_conditional=[
+                    {'if': {'column_id': 'autores'},
+                    'width': '300px'},
+                    {'if': {'column_id': 'institucion'},
+                    'width': '300px'},
+                ],
                 style_cell={
-                    'maxWidth': '300px',
+                    'maxHeight': '50px',
                     'overflow': 'hidden',
                     'whiteSpace': 'normal',
                     'textAlign': 'left',
@@ -68,14 +80,15 @@ def actualizar_elemento_seleccionado(elemento, fuente):
                 id='filter_feature',
                 options = [],
                 disabled =True,
-                value = None
+                value = None,
+                placeholder='Elemento Requerido'
             ),
         id='div_feature'),
         html.H5("Entrada:",className="title_white",style={"color":"white"}),
         html.Div(children=[
             dcc.Input(
                 id='filter_input',
-                placeholder='Digite el filtro',
+                placeholder='Característica Requerida',
                 type='text',
                 disabled =True,
                 value = None
@@ -88,14 +101,14 @@ def actualizar_elemento_seleccionado(elemento, fuente):
             dcc.Dropdown(
                 id='filter_feature',
                 options = opciones_caracteristica,
-                value = None             
+                value = None,             
             ),
         id='div_feature'),
         html.H5("Entrada:",className="title_white",style={"color":"white"}),
         html.Div(children=[
             dcc.Input(
                 id='filter_input',
-                placeholder='Inactivo',
+                placeholder='Característica Requerida',
                 type='text',
                 disabled =True,
                 value = None
@@ -114,19 +127,18 @@ def actualizar_caractersitica_seleccionada(caracteristica,elemento,fuente):
         valor_entrada, opciones_entrada=filtrar_caracteristica(caracteristica,elemento,fuente)
     
     if type(valor_entrada) == str:
-        filter =  dcc.Input(id='filter_input', placeholder='Digite el filtro', type='text', value='')
+        filter =  dcc.Input(id='filter_input', placeholder='', type='text', value='')
     elif type(valor_entrada) == list:
         filter = dcc.Dropdown(id="filter_input", options= opciones_entrada, multi=True)
     elif type(valor_entrada) == tuple:
-        year_today = date.today().year
-        option_drop = list(range(1975,date.today().year))
+        #year_today = date.today().year
+        option_drop = list(range(1975,date.today().year+1))
         print('drop: ',option_drop)   
         #filter = dbc.Input(id="date_start", type="number", min=1975, max=year_today, step=1, placeholder='Año inicial'), dcc.Input(id="date_end", type="number", min=0, max=year_today, step=1 , disabled = True, placeholder='Año final'), dcc.Input(id='filter_input', style={'display': 'none'})
         filter = dcc.Dropdown(id="date_start", placeholder='Año inicial', options = option_drop, value=None), dcc.Dropdown(id="date_end", options= [],disabled = True, placeholder='Año final', value= None), dcc.Input(id='filter_input', style={'display': 'none'})
 
     else:
-        print('entro aqui')
-        filter = dcc.Input(id='filter_input', placeholder='Inactivo', value='', disabled=True)
+        filter = dcc.Input(id='filter_input', placeholder='Característica Requerida', value='', disabled=True)
     return filter
 
 @callback(
@@ -134,9 +146,9 @@ def actualizar_caractersitica_seleccionada(caracteristica,elemento,fuente):
      Input('date_start', 'value')
  )
 def validate_date_end(minimo):
-     if minimo == None:
-         return [1975,date.today().year], True
-     return list(range(minimo,date.today().year)), False
+    if minimo == None:
+        return [1975,date.today().year], True
+    return list(range(minimo,date.today().year+1)), False
 
 @callback(
     Output('filter_input', 'value'),
@@ -150,7 +162,7 @@ def filter_input_contructor(inicial, final):
         fechas = str((inicial, final))
     return fechas
 
-@callback([Output('table_date', 'data'),Output('table_date', 'columns')],
+@callback([Output('table_date', 'data'),Output('table_date', 'columns'),Output("auto-toast", "is_open")],
           [Input('button_state','n_clicks'),
             State('filter_fuente', 'value'),
             State('filter_element', 'value'),
@@ -168,38 +180,35 @@ def display(boton,fuente, elemento, caracteristica, entrada):
         pass
     if (elemento==None) and (caracteristica==None) and ((entrada==None) or (entrada=='')):
         data=pd.DataFrame()
-        columns=None
         #tool_tip=[]
     elif (elemento != None) and (caracteristica==None) and ((entrada==None) or (entrada=='')):
         data = filtrar_elemento(elemento,fuente,'data')
-        columns=[{'name': i, 'id': i} for i in data.columns]
-        data=data
         #tool_tip=[{str(column): {'value': str(value), 'type': 'text'} for column, value in row.items()} for row in data]
     elif (elemento !=None) and (caracteristica != None) and ((entrada!=None) and (entrada!='')):
         data = filtrar_entrada(entrada,caracteristica,elemento,fuente)
-        columns=[{'name': i, 'id': i} for i in data.columns]
-        data=data
         #tool_tip=[{str(column): {'value': str(value), 'type': 'text'} for column, value in row.items()} for row in data]
     elif (elemento!= None) and (caracteristica !=None) and ((entrada==None) or (entrada=='')):
         data = filtrar_elemento(elemento,fuente,'data')
-        columns=[{'name': i, 'id': i} for i in data.columns]
-        data=data
     else:
         data=pd.DataFrame()
-        columns=None
-        #tool_tip=[]
-    
-    if data.shape[0]>1:
+    print(data.shape[0])
+    if data.shape[0]>=1:
         data=globals()[str(referencias[fuente][elemento])].loc[list(data.index)].astype(str).fillna('No Aplica')
         if fuente=='SCOPUS':
-            data=data.drop(['scopus_id', 'pais','eid','issue','numero_articulo','pag_inicio','pag_fin','pag_count','affil_id','abstract','etapa_publicacion','autores_id'], axis=1, errors='ignore')
+            data=data.drop(['scopus_id','eid','issue','numero_articulo','pag_inicio','pag_fin','pag_count','affil_id','abstract','etapa_publicacion','autores_id'], axis=1, errors='ignore')
             locs=data[data['institucion'].str.len()>300].index.tolist()
             data['institucion'].loc[locs]=data['institucion'].loc[locs].str.slice(stop=300)+'...'
             locs=data[data['autores'].str.len()>300].index.tolist()
-            data['autores'].loc[locs]=data['autores'].loc[locs].str.slice(stop=300)+'...'
-            #data['pais']=data[data['pais'].str.len()>300]['pais'].str.slice(stop=300)+'...'
+            data['autores'].loc[locs]=data['autores'].loc[locs].str.slice(stop=200)+'...'
+            locs=data[data['pais'].str.len()>300].index.tolist()
+            data['pais'].loc[locs]=data['pais'].loc[locs].str.slice(stop=300)+'...'
         else:
             data=data.drop(['volumen','fasciculo','paginas'], axis=1, errors='ignore')
+        columns=[{'name': i, 'id': i} for i in data.columns]
+        toast=False
+    else:
+        columns=None
+        toast=True
         
-    return data.to_dict('records'),columns#,tool_tip
+    return data.to_dict('records'),columns, toast
 
