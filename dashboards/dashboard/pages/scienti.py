b06c1e6aac2cc5acf0150a09a9a5e3a6e30ccc0d
JarbyDaniel <dsalazarg@unicauca.edu.co>
2023-06-14 16:56:55 -0500

    base

--- dashboards/dashboard/pages/scienti.py
+++ dashboards/dashboard/pages/scienti.py
@@ -31,46 +31,46 @@ def render_content(tab):
             id="loading2",
             # children=[html.H2("Información filtrado individual (eliminar msj)", className="title_graph_main"),                
                 children=[html.Div([                    
-                    html.H1(children="Indicadores del grupo de investigación: ", id="indicators_group", className="group_graph_info"),
-                    html.H1(children="Todos los productos: ",id="products_element_group", className="group_graph_info"),
+                    html.H1(children="Indicadores para grupos de investigación: ", id="indicators_group", className="group_graph_info"),
+                    html.H1(children="Por favor selecciones elementos a filtrar",id="products_element_group", className="group_graph_info"),
                     html.Div([
-                        html.Div([html.H1("Indicador 1", style={'fontSize':'1.3rem'}),html.P(children='0', id='kpi-1',className='badge')], className='score-card'),
-                        html.Div([html.H1("Indicador 2", style={'fontSize':'1.3rem'}),html.P(children='0', id='kpi-2',className='badge')], className='score-card'),
-                        html.Div([html.H1("Indicador 3", style={'fontSize':'1.3rem'}),html.P(children='0', id='kpi-3',className='badge')], className='score-card'),
-                        html.Div([html.H1("Indicador 4", style={'fontSize':'1.3rem'}),html.P(children='0', id='kpi-4',className='badge')], className='score-card'),
-                        html.Div([html.H1("Indicador 5", style={'fontSize':'1.3rem'}),html.P(children='0', id='kpi-5',className='badge')], className='score-card'),
+                        html.Div([html.H1("Consistencia", style={'fontSize':'1.3rem'}),html.P(children='0', id='kpi-1',className='badge')], className='score-card'),
+                        html.Div([html.H1("PPA", style={'fontSize':'1.3rem'}),html.P(children='0', id='kpi-2',className='badge')], className='score-card'),
+                        html.Div([html.H1("PPUA", style={'fontSize':'1.3rem'}),html.P(children='0', id='kpi-3',className='badge')], className='score-card'),
+                        html.Div([html.H1("PG", style={'fontSize':'1.3rem'}),html.P(children='0', id='kpi-4',className='badge')], className='score-card'),
+                        html.Div([html.H1("Autores", style={'fontSize':'1.3rem'}),html.P(children='0', id='kpi-5',className='badge')], className='score-card'),
                     ],className='card-score-targets'),
                     html.H1(html.Span(dcc.Link("Abrir enlace del grupo en GrupLac", href="", id="url_group_grouplac",target="_blank", className="group_graph_url", style={"color":"#08469b"}))),
                     html.H1(html.Span(dcc.Link("Abrir indicadores del grupo en Minciencias", id="group_minciencias", href="", target="_blank", className="group_graph_url",style={"color":"#08469b"}))),
 
-                ],className='card-score-container'),
+                ],id="kpi_all" ,className='card-score-container', style={'display':'none'}),
                 #Charts or Graphs
                 html.Div([
-                    dcc.Graph(figure={}, id='dash_individual_graph1',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":False})
-                ],id='div_group_figure1', className='card-graph card-body col-graph-big'),             
+                    dcc.Graph(figure={}, id='dash_individual_graph1',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":True})
+                ],id='div_group_figure1', className='card-graph card-body col-graph-big', style={'display':'none'}),             
                 html.Div([
                     html.Div([                        
-                        dcc.Graph(figure={}, id='dash_individual_graph2',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":False})
-                    ],id='div_group_figure2', className='card-graph_middle card-body col-graph-middle'),
+                        dcc.Graph(figure={}, id='dash_individual_graph2',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":True})
+                    ],id='div_group_figure2', className='card-graph_middle card-body col-graph-middle', style={'display':'none'}),
                     html.Div([
-                        dcc.Graph(figure={}, id='dash_individual_graph3',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":False})
-                    ],id='div_group_figure3', className='card-graph_middle card-body col-graph-middle'),
+                        dcc.Graph(figure={}, id='dash_individual_graph3',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":True})
+                    ],id='div_group_figure3', className='card-graph_middle card-body col-graph-middle', style={'display':'none'}),
                 ],className='col-graph-father'),
                 html.Div([
-                    dcc.Graph(figure={}, id='dash_individual_graph4',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":False})
-                ],id='div_group_figure4', className='card-graph card-body col-graph-big'),
-            ],type="cube", fullscreen=False, style={'height':'100%', 'marginTop':'15rem','textAlign':'center', 'display':'flex', 'justifyContent':'space-around',"color":"black"}
+                    dcc.Graph(figure={}, id='dash_individual_graph4',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":True})
+                ],id='div_group_figure4', className='card-graph card-body col-graph-big', style={'display':'none'}),
+            ], type="cube", fullscreen=False, style={'height':'100%', 'marginTop':'15rem','textAlign':'center', 'display':'flex', 'justifyContent':'space-around',"color":"black"}
         )
     else:
         return dcc.Loading(
             id="loading1",
             children=[html.H2("Indicadores General", className="title_graph_main"),
                 html.Div([
-                html.Div([html.Div([html.H1("Grafica 1"),dcc.Graph(id='violin-plot',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":False})],className='card-graph card-body')], className='col-graph-big'),
-                html.Div([html.Div([html.H1("Grafica 2"),dcc.Graph(id='pie-1',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":False})],className='card-graph card-body')], className='col-graph-big'),
-                html.Div([html.Div([html.H1("Grafica 3 media pantalla"),dcc.Graph(id='histogram',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":False})], className='card-graph card-body')], className='col-graph_50'),
-                html.Div([html.Div([html.H1("Grafica 4 pantalla completa"),dcc.Graph(id='path',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":False})], className='card-graph card-body')], className='col-graph_100'),
-                html.Div([html.Div([html.H1("Grafica 5"),dcc.Graph(id='radial',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":False})], className='card-graph card-body')], className='col-graph'),
-                html.Div([html.Div([html.H1("Grafica 6"),dcc.Graph(id='radial',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":False})], className='card-graph card-body')], className='col-graph'),  
+                html.Div([html.Div([html.H1("Grafica 1"),dcc.Graph(id='violin-plot',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":True})],className='card-graph card-body')], className='col-graph-big'),
+                html.Div([html.Div([html.H1("Grafica 2"),dcc.Graph(id='pie-1',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":True})],className='card-graph card-body')], className='col-graph-big'),
+                html.Div([html.Div([html.H1("Grafica 3 media pantalla"),dcc.Graph(id='histogram',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":True})], className='card-graph card-body')], className='col-graph_50'),
+                html.Div([html.Div([html.H1("Grafica 4 pantalla completa"),dcc.Graph(id='path',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":True})], className='card-graph card-body')], className='col-graph_100'),
+                html.Div([html.Div([html.H1("Grafica 5"),dcc.Graph(id='radial',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":True})], className='card-graph card-body')], className='col-graph'),
+                html.Div([html.Div([html.H1("Grafica 6"),dcc.Graph(id='radial',style={'Width':'100%', "height":'100%'}, config={"displaylogo":False, "displayModeBar":True})], className='card-graph card-body')], className='col-graph'),  
             ])],type="cube", fullscreen=False, style={'height':'100%', 'marginTop':'15rem','textAlign':'center', 'display':'flex', 'justifyContent':'space-around',"color":"black"},
         )
