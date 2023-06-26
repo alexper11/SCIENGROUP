027ef491e61a443c46b602824ed90d95430ec8eb
JarbyDaniel <dsalazarg@unicauca.edu.co>
2023-06-26 11:37:59 -0500

    base estilos

--- dashboards/dashboard/pages/homepage.py
+++ dashboards/dashboard/pages/homepage.py
@@ -19,26 +19,27 @@ layout = html.Div(
                 'padding': '3em',
                 'border-radius': '1em',
                 'margin': '5vh auto',
-                'max-width': '50%'
+                'max-width': '70%'
             },
             children=[
                 html.H3(
-                    'Analizador de producción científica de investigadores',
+                    'Herramienta de análisis y visualización bibliométrica dentro del ámbito de los grupos de investigación en el Cauca.',
                     style={
                         'color': 'black',
                         'text-align': 'center',
                         'font-size': '1.5em'
                     }
                 ),
-                html.H3(
-                    'Este dashboard es una herramienta muy útil que permite presentar y analizar métricas clave,como el número de publicaciones, citas, índices de impacto, colaboraciones, etc. Permite Visualización clara de los datos, Fácil identificación de áreas de mejora, Seguimiento del progreso a lo largo del tiempo, Identificación de oportunidades de colaboración. Este dashboard es una herramienta muy útil que permite presentar y analizar métricas clave,como el número de publicaciones, citas, índices de impacto, colaboraciones, etc. Permite Visualización clara de los datos, Fácil identificación de áreas de mejora, Seguimiento del progreso a lo largo del tiempo, Identificación de oportunidades de colaboración. Este dashboard es una herramienta muy útil que permite presentar y analizar métricas clave,como el número de publicaciones, citas, índices de impacto, colaboraciones, etc. Permite Visualización clara de los datos, Fácil identificación de áreas de mejora, Seguimiento del progreso a lo largo del tiempo, Identificación de oportunidades de colaboración',
+                html.P([
+                    'Este dashboard es una herramienta muy útil que permite presentar y analizar métricas clave,como el número de publicaciones, citas, índices de impacto, colaboraciones, etc. Permite Visualización clara de los datos, Fácil identificación de áreas de mejora, Seguimiento del progreso a lo largo del tiempo, Identificación de oportunidades de colaboración. Este dashboard es una herramienta muy útil que permite presentar y analizar métricas clave,como el número de publicaciones, citas, índices de impacto, colaboraciones, etc. Permite Visualización clara de los datos, Fácil identificación de áreas de mejora, Seguimiento del progreso a lo largo del tiempo, Identificación de oportunidades de colaboración. Este dashboard es una herramienta muy útil que permite presentar y analizar métricas clave,como el número de publicaciones, citas, índices de impacto, colaboraciones, etc. Permite Visualización clara de los datos, Fácil identificación de áreas de mejora, Seguimiento del progreso a lo largo del tiempo, Identificación de oportunidades de colaboración ',
+                    'actores del SRCTI del Cauca para aportar valor a la sociedad a partir del conocimiento'],
                     style={
                         'color': 'black',
-                        'text-align': 'center',
-                        'font-size': '1.2em',
+                        'font-size': '1.3em',
                         'text-align': 'justify'
                     }
-                )
+                ),
+                html.Img(src='./assets/img/srcti.png', id='options_graph',style={'width':'auto', "height":'25rem', "padding-left":'auto',"padding-right":'auto', "position":"static"}),
             ]
         )
     ]
