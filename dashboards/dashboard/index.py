273300d813ed687a4e8d2d4592296a269c77ee63
JarbyDaniel <dsalazarg@unicauca.edu.co>
2023-06-27 15:11:16 -0500

    anchura fija hist

--- dashboards/dashboard/index.py
+++ dashboards/dashboard/index.py
@@ -132,5 +132,5 @@ def loadpage(path_route):
         return page404.layout
 
 if __name__ == '__main__':
-    #app.run_server(host='10.55.4.73', port=8051,debug=False)
-    app.run_server(host='localhost', port=8051,debug=True)
+    app.run_server(host='10.55.4.73', port=8051,debug=False)
+    #app.run_server(host='localhost', port=8051,debug=True)
